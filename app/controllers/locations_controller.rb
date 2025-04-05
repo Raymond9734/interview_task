509e247a70ea8897185d5f6e72d0d68e6b58c90b
Raymond9734 <rymadara97@gmail.com>
2025-04-05 16:24:30 +0300

    Refactor LocationsController to include user-specific location data and debug logging for Inertia.js rendering

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,16 +4,39 @@ class LocationsController < ApplicationController
   before_action :authorize_user, only: [:edit, :update, :destroy]
 
   def index
-    locations = Location.all.includes(:user)
-    render inertia: 'Locations/Index', props: { locations: locations }
+    # Add debug logs
+    Rails.logger.debug "Current user: #{current_user.inspect}"
+    
+    all_locations = Location.all.includes(:user)
+    @user_locations = all_locations.where(user: current_user)
+    @other_locations = all_locations.where.not(user: current_user)
+    
+    Rails.logger.debug "User locations: #{@user_locations.inspect}"
+    Rails.logger.debug "Other locations: #{@other_locations.inspect}"
+    
+    render inertia: 'Locations/Index', props: { 
+      user_locations: @user_locations.as_json(include: { user: { only: [:id, :username, :role] } }),
+      other_locations: @other_locations.as_json(include: { user: { only: [:id, :username, :role] } }),
+      user: current_user
+    }
   end
 
   def show
-    render inertia: 'Locations/Show', props: { location: @location }
+    @location = Location.includes(:user).find(params[:id])
+    all_locations = Location.all.includes(:user)
+    render inertia: 'Locations/Show', props: { 
+      location: @location.as_json(include: { user: { only: [:id, :username, :role] } }),
+      user: current_user,
+      allLocations: all_locations.as_json(include: { user: { only: [:id, :username, :role] } })
+    }
   end
 
   def new
-    render inertia: 'Locations/New'
+    all_locations = Location.all.includes(:user)
+    render inertia: 'Locations/New', props: {
+      user: current_user,
+      allLocations: all_locations.as_json(include: { user: { only: [:id, :username, :role] } })
+    }
   end
 
   def create
@@ -25,14 +48,21 @@ class LocationsController < ApplicationController
       render inertia: 'Locations/New', 
              props: { 
                errors: location.errors.messages,
-               location: location_params
+               location: location_params,
+               user: current_user
              }, 
              status: :unprocessable_entity
     end
   end
 
   def edit
-    render inertia: 'Locations/Edit', props: { location: @location }
+    @location = Location.includes(:user).find(params[:id])
+    all_locations = Location.all.includes(:user)
+    render inertia: 'Locations/Edit', props: { 
+      location: @location.as_json(include: { user: { only: [:id, :username, :role] } }),
+      user: current_user,
+      allLocations: all_locations.as_json(include: { user: { only: [:id, :username, :role] } })
+    }
   end
 
   def update
@@ -42,7 +72,8 @@ class LocationsController < ApplicationController
       render inertia: 'Locations/Edit', 
              props: { 
                errors: @location.errors.messages,
-               location: @location
+               location: @location,
+               user: current_user
              }, 
              status: :unprocessable_entity
     end
