c9d11bd4202b7b78e00fa8caf5fe68ce8e5f4616
Raymond9734 <rymadara97@gmail.com>
2025-04-05 14:21:54 +0300

    Add DashboardController to manage user administration and authentication

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,28 @@
+class DashboardController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_admin
+
+  def index
+    users = User.all.includes(:locations)
+    render inertia: 'Dashboard/Index', props: { users: users }
+  end
+
+  def destroy_user
+    user = User.find(params[:id])
+    
+    if user.admin?
+      redirect_to dashboard_path, alert: 'Cannot delete admin users.'
+    else
+      user.destroy
+      redirect_to dashboard_path, notice: 'User was successfully deleted.'
+    end
+  end
+
+  private
+
+  def require_admin
+    unless current_user.admin?
+      redirect_to root_path, alert: 'You are not authorized to access this page.'
+    end
+  end
+end 
