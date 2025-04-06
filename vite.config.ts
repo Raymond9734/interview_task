a8f5585c64dff62d1dd384a51edec6ee8da87456
Raymond9734 <rymadara97@gmail.com>
2025-04-06 13:11:51 +0300

    Remove Tailwind CSS plugin from Vite configuration, and add build options for sourcemaps and manifest generation to enhance build process.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     RubyPlugin(),
   ],
+  build: {
+    sourcemap: false,
+    manifest: true
+  },
   server: {
-    hmr: false  // Disable HMR
+    hmr: false
   }
 })
