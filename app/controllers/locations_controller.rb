0bf31d626ef3b154e07520d3c805df85a8729687
Raymond9734 <rymadara97@gmail.com>
2025-04-07 19:25:54 +0300

    Add comments to LocationsController for improved clarity on actions and methods

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,13 @@
+# LocationsController manages CRUD operations for user-created locations.
+# It also provides views for personal and public locations, with authorization checks.
 class LocationsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_location, only: [ :show, :edit, :update, :destroy ]
   before_action :authorize_user, only: [ :edit, :update, :destroy ]
 
 
-
+  # GET /locations/:id
+  # Shows a single location along with all locations
   def show
     @location = Location.includes(:user).find(params[:id])
     all_locations = Location.all.includes(:user)
@@ -15,6 +18,8 @@ class LocationsController < ApplicationController
     }
   end
 
+  # GET /locations/new
+  # Renders form to create a new location
   def new
     all_locations = Location.all.includes(:user)
     render inertia: "Locations/New", props: {
@@ -23,6 +28,8 @@ class LocationsController < ApplicationController
     }
   end
 
+  # POST /locations
+  # Handles creation of a new location
   def create
     location = current_user.locations.build(location_params)
 
@@ -44,6 +51,8 @@ class LocationsController < ApplicationController
     end
   end
 
+  # GET /locations/:id/edit
+  # Renders form to edit an existing location
   def edit
     @location = Location.includes(:user).find(params[:id])
     all_locations = Location.all.includes(:user)
@@ -54,6 +63,8 @@ class LocationsController < ApplicationController
     }
   end
 
+  # PATCH/PUT /locations/:id
+  # Updates an existing location
   def update
     if @location.update(location_params)
       redirect_to locations_path, notice: "Location was successfully updated."
@@ -68,11 +79,15 @@ class LocationsController < ApplicationController
     end
   end
 
+  # DELETE /locations/:id
+  # Deletes a location if the user is authorized
   def destroy
     @location.destroy
     redirect_to locations_path, notice: "Location was successfully deleted."
   end
 
+  # GET /locations/my
+  # Shows only locations created by the current user
   def my
     render inertia: "Locations/MyLocations", props: {
       user_locations: current_user.locations.as_json(include: { user: { only: [ :id, :username, :role ] } }),
@@ -80,6 +95,9 @@ class LocationsController < ApplicationController
     }
   end
 
+  # GET /locations/others
+  # Shows locations created by users other than the current user
+  # Includes search and pagination (4 per page)
   def others
     query = Location.where.not(user: current_user)
                    .includes(:user)
@@ -106,6 +124,8 @@ class LocationsController < ApplicationController
     }
   end
 
+  # GET /locations
+  # Lists all locations in the system
   def index
     all_locations = Location.all.includes(:user)
     render inertia: "Locations/Index", props: {
@@ -116,6 +136,7 @@ class LocationsController < ApplicationController
 
   private
 
+  # Finds the location based on the ID paramete
   def set_location
     @location = Location.find(params[:id])
   end
@@ -124,6 +145,7 @@ class LocationsController < ApplicationController
     params.require(:location).permit(:name, :latitude, :longitude)
   end
 
+  # Ensures only the location's owner or an admin can edit/update/delete it
   def authorize_user
     unless @location.user == current_user || current_user.admin?
       redirect_to locations_path, alert: "You are not authorized to perform this action."
