21454f0733aa7092b6c74fb8416e303066d0b1dd
Raymond9734 <rymadara97@gmail.com>
2025-04-07 07:17:56 +0300

    Update 'Locations' link in Home page to point to 'My Locations' for improved user navigation consistency.

--- app/frontend/pages/Home/Index.jsx
+++ app/frontend/pages/Home/Index.jsx
@@ -122,7 +122,7 @@ export default function Home({ locations, user }) {
                         Add New Location
                       </Link>
                       <Link
-                        href="/locations"
+                        href="/locations/my"
                         className="inline-flex items-center justify-center px-4 py-2 border border-amber-300/50 text-sm font-medium rounded-xl text-amber-800 bg-white/60 hover:bg-amber-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-600 transform transition-all duration-300 hover:scale-[1.02]"
                       >
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
