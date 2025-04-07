902b5e5d2c78ca7eca36a3801fb9a398834f50b3
Raymond9734 <rymadara97@gmail.com>
2025-04-07 19:25:46 +0300

    Add comments to HomeController for improved clarity on actions and methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
+# HomeController manages the landing/homepage for authenticated users.
+# It loads and renders location data along with associated user details.
 class HomeController < ApplicationController
   before_action :authenticate_user!
 
+  # GET /home
+  # Renders a list of all locations with their associated user data.
   def index
     locations = Location.all.includes(:user)
     render inertia: "Home/Index", props: {
