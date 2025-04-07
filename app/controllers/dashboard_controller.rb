14280b7ebe53ca752a7217cd38ab56db1ed298b2
Raymond9734 <rymadara97@gmail.com>
2025-04-07 09:54:23 +0300

    Implement pagination and search functionality in DashboardController index action. Added support for paginated user retrieval and included pagination metadata in the response for improved user experience.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,10 +3,26 @@ class DashboardController < ApplicationController
   before_action :require_admin
 
   def index
+    page = params[:page] || 1
+    per_page = params[:per_page] || 10
+    
     users = User.all.includes(:locations)
+                .search_by_term(params[:search])
+    
+    total_count = users.count
+    
+    paginated_users = users.offset((page.to_i - 1) * per_page.to_i)
+                          .limit(per_page.to_i)
+
     render inertia: 'Dashboard/Index', props: { 
-      users: users.as_json(methods: [:locations_count], except: [:password_digest]),
-      user: current_user.as_json(methods: [:locations_count], except: [:password_digest])
+      users: paginated_users.as_json(methods: [:locations_count], except: [:password_digest]),
+      user: current_user.as_json(methods: [:locations_count], except: [:password_digest]),
+      pagination: {
+        total: total_count,
+        per_page: per_page.to_i,
+        current_page: page.to_i,
+        total_pages: (total_count.to_f / per_page.to_i).ceil
+      }
     }
   end
 
@@ -28,4 +44,4 @@ class DashboardController < ApplicationController
       redirect_to root_path, alert: 'You are not authorized to access this page.'
     end
   end
-end 
+end
